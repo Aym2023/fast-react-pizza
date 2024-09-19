997c69968103f7fc04361df7101e07c426c83381
ayman saleh <ayman.saleh000@gmail.com>
2024-09-19 22:55:43 +0300

    modyfing

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './ui/Home';
-import Menu from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Applayout from "./ui/Applayout";
 
 const router = createBrowserRouter([
-{
-path: '/',
-element: <Home />,
-},
-{
-path: '/menu',
-element: <Menu />,
-},
-{
-path: '/cart',
-element: <Cart />
-},
-{
-path: '/oeder/new',
-element: <createRouter />
-},
-{
-path: '/order/new',
-element: <CreateOrder />
-},
-{
-path: '/order/:orderid',
-element:  <Order />
-},    
-    
+  {
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/oeder/new",
+        element: <createRouter />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderid",
+        element: <Order />,
+      },
+    ],
+  },
 ]);
 
 function App() {
- 
-    return <RouterProvider router={router}/>;  
+  return <RouterProvider router={router} />;
 }
 
 export default App;
-
-
