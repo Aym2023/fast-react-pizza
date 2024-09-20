c5578fa08276a3cacc97b6489e2dc4b8c2b08e58
ayman saleh <ayman.saleh000@gmail.com>
2024-09-20 14:55:26 +0300

    modyfing

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,4 +1,5 @@
-import { formatCurrency } from "../../utils/helpers";
+import { formatCurrency } from "../../utilites/helpers";
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
