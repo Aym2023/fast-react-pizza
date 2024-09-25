775e1cc27ba162aa43f7379267cf5e74b6477ab6
ayman saleh <ayman.saleh000@gmail.com>
2024-09-25 10:46:07 +0300

    modyfied

---
+++ src/ui/prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+};
+
+// .prettierrc
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./pages/**/*.{html,js}',
-    './components/**/*.{html,js}',],
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
