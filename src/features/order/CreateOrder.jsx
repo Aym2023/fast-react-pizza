ac6ba31acdad9205d9044c1e1ff879fdbf5d5d78
ayman saleh <ayman.saleh000@gmail.com>
2024-09-29 16:21:51 +0300

    modified

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -50,13 +50,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer"  className="input" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input"/>
           </div>
           { formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -68,9 +68,7 @@ function CreateOrder() {
             type="text" 
             name="address"
             required 
-            className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 w-full md:px-6 md:py-3
-            "
-
+            className="input"
              />
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports =  {
   content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
 
+// module.exports = {
+//   mode: "jit",
+//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   // Other Tailwind configurations...
+// };
