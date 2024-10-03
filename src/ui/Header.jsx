8e7e85f0eb00823c0e42dddc1bddcc594b430087
ayman saleh <ayman.saleh000@gmail.com>
2024-10-03 19:22:55 +0300

    modyfied

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../features/user/userName";
 
 function Header() {
     return (
-        <header className=" flex justify-between border-b border-stone-200 px-4 py-3  bg-pizza uppercase sm:px-6n  text-sm"  
+        <header className=" flex justify-between border-b border-stone-200 px-4 py-3  bg-pizza uppercase sm:px-6n bg-yellow-400  text-sm"  
          >
             <Link to='/' className="tracking-widest" >fast react pizza Co.</Link>
 
